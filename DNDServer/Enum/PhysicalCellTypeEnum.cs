1741afafb519481bdfc2acdfc059b5c0124b8ffa
Mustafa Mustafaiev <mustafaev.m2003@gmail.com>
2025-01-06 17:19:00 +0200

    Add project files.

---
+++ DNDServer/Enum/PhysicalCellTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDServer.Enum
+{
+    public enum PhysicalCellTypeEnum
+    {
+        Water,
+        Earth,
+        Mountain
+    }
+}
---
+++ DNDServer/Model/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDServer.Model
+{
+    public class Cell
+    {
+        public int X; 
+        public int Y;
+    }
+}
